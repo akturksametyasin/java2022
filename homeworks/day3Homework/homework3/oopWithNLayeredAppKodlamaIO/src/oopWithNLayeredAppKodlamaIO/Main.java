f3790a58efad38893926d9a8a0f49c6cd948dd70
Samet Yasin Aktürk <78413314+akturksametyasin@users.noreply.github.com>
2022-10-15 22:06:00 +0300

    day3Homework

--- homeworks/day3Homework/homework3/oopWithNLayeredAppKodlamaIO/src/oopWithNLayeredAppKodlamaIO/Main.java
+++ homeworks/day3Homework/homework3/oopWithNLayeredAppKodlamaIO/src/oopWithNLayeredAppKodlamaIO/Main.java
@@ -25,13 +25,8 @@ public class Main {
 		Category category1 = new Category("category1", "Front-end");
 		categoryManager.add(category1);
 		
-		System.out.println("-----------------------------------------------");
-		
 		Category category2 = new Category("category2", "Back-end");
 		categoryManager.add(category2);
-		
-		
-		System.out.println("*******************************************************************");
 		
 		
 		CourseManager courseManager = new CourseManager(new HibernateCourseDao(), loggers);
@@ -39,13 +34,8 @@ public class Main {
 		Course course1 = new Course("course4", "Programlamaya Giriş İçin Temel Kurs", "programlamaGiris.png", "PYTHON, JAVA, C# gibi tüm programlama dilleri için temel programlama mantığını anlaşılır örneklerle öğrenin.", 0); 
 		courseManager.add(course1);
 		
-		System.out.println("-----------------------------------------------");
-		
 		Course course2 = new Course("course5", "Yazılım Geliştirici Yetiştirme Kampı (C# + Angular)", "cAndAngular.png", "2 ay sürecek Yazılım Geliştirici Yetiştirme Kampımızın takip, doküman ve duyurularını buradan yapacağız.", 0);		
 		courseManager.add(course2);
-		
-		
-		System.out.println("*******************************************************************");
 		
 		
 		InstructorManager instructorManager = new InstructorManager(new JdbcInstructorDao(), loggers);
